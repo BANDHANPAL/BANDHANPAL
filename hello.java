219bee0b0d3278d90ed16bda8be9fbd892de56af
BANDHAN PAL <50814816+BANDHANPAL@users.noreply.github.com>
2021-03-18 02:19:11 +0530

    Update hello.java

--- hello.java
+++ hello.java
@@ -1,7 +1,7 @@
 public class hello{
     public static void main(String[] args) {
         for(int i=0;i<=10;i++)
-        System.out.println("Hi Welcome hii this is bandhan"+i);
+        System.out.println("Hi Welcome hii this is bandhan welcome"+i);
     }
 
 }
